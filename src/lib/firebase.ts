ebbec34b7aa580f3888d2da35e101924907f7d66
Thiago Domingos <thiago@sagacy.com.br>
2025-08-02 03:10:29 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // IMPORTANT: Replace this with your actual Firebase project configuration
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_YOUR_API_KEY",
-  authDomain: "REPLACE_WITH_YOUR_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_YOUR_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_YOUR_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_YOUR_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_YOUR_APP_ID",
-  measurementId: "REPLACE_WITH_YOUR_MEASUREMENT_ID"
+  "projectId": "visitas-sagacy",
+  "appId": "1:835253296541:web:8ada22aab3e879c27b2374",
+  "storageBucket": "visitas-sagacy.firebasestorage.app",
+  "apiKey": "AIzaSyD8A9QypKybW_OseOptedl8geQwK4BiOh8",
+  "authDomain": "visitas-sagacy.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "835253296541"
 };
 
 const app = initializeApp(firebaseConfig);
